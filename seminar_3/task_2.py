96fedde1b3a6162c8e13c971796652d888df3d26
Rina Antropova <rin.antropova@gmail.com>
2024-06-18 10:35:22 +0300

    sem_3, task_2 - seminar's solutions

--- seminar_3/task_2.py
+++ seminar_3/task_2.py
@@ -5,6 +5,7 @@
 # Input: [1, 2, 3, 4, 5] k = 3
 # Output: [4, 5, 1, 2, 3]
 
+# My solution:
 values = input('Enter list of numbers, separated by comma: ')
 k = int(input('Enter positive number k: '))
 numbers = list(map(int, values.split(",")))
@@ -18,3 +19,21 @@ def rotate(arr, a):
 
 rotated_numbers = rotate(numbers, k)
 print(rotated_numbers)
+
+
+
+# Seminar solution 1:
+my_list = [1, 2, 3, 4, 5]
+k = 3
+new_list = my_list[k:] + my_list[:k]
+print(new_list)
+
+
+# Seminar solution with loops:
+my_list = [1, 2, 3, 4, 5]
+k = 3
+for _ in range(k-1):
+    last_el = my_list.pop()
+    my_list.insert(0, last_el)
+
+print(my_list)

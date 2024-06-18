63d932f6643980a3934db83c9691cda03880e207
Rina Antropova <rin.antropova@gmail.com>
2024-06-18 10:24:34 +0300

    sem_3, task_1 - seminar's solutions

--- seminar_3/task_1.py
+++ seminar_3/task_1.py
@@ -13,3 +13,32 @@ unique_count = len(set(numbers))
 print(unique_count)
 
 
+# # Seminar solution:
+my_list = [1, 1, 2, 0, -1, 3, 4, 4]
+print(len(set(my_list)))
+
+
+# Seminar solution with while loop and try-except:
+# new_list = []
+# # flag = True
+# while True:
+#     try:
+#         my_str = int(input('Enter a number, to stop input enter any symbol: '))
+#     except ValueError:
+#         print('You entered a string')
+#         break
+#     else:
+#         new_list.append(my_str)
+#
+#
+# print('Initial list: \n', new_list)
+# print(len(set(new_list)))
+
+
+# Seminar solution with for loop
+my_list = [1, 1, 2, 0, -1, 3, 4, 4]
+res = []
+for el in my_list: # арифметический
+    if el not in res:
+        res.append(el)
+print(len(res))
